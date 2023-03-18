d577bc1250bfc10aa4ad0abd2102a96124cd0510
I Putu Saputrayana <ikhyan7@gmail.com>
2023-03-18 15:46:59 +0800

    update content (#1)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,12 @@
-import Link from 'next/link'
-import ThemeChanger from './DarkSwitch'
-import { Disclosure } from '@headlessui/react'
+import Link from 'next/link';
+import { Disclosure, Transition } from '@headlessui/react';
 
 export default function Navbar() {
-  const navigation = ['Product', 'Features', 'Pricing', 'Company', 'Blog']
+  const navigation = ['Product', 'Features', 'Pricing', 'Company', 'Blog'];
 
   return (
-    <div className="w-full">
-      <nav className="container relative flex flex-wrap items-center justify-between p-8 mx-auto lg:justify-between xl:px-0">
+    <div className="w-full backdrop-blur-md bg-neutral-900/30 sticky top-0 left-0 z-10">
+      <nav className="container relative flex flex-wrap items-center justify-between px-8 py-4 mx-auto lg:justify-between xl:px-0">
         {/* Logo  */}
         <Disclosure>
           {({ open }) => (
@@ -27,7 +26,11 @@ export default function Navbar() {
                   aria-label="Toggle Menu"
                   className="px-2 py-1 ml-auto text-gray-500 rounded-md lg:hidden hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:text-gray-300 dark:focus:bg-trueGray-700"
                 >
-                  <svg className="w-6 h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <svg
+                    className="w-6 h-6 fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
                     {open && (
                       <path
                         fillRule="evenodd"
@@ -43,27 +46,36 @@ export default function Navbar() {
                     )}
                   </svg>
                 </Disclosure.Button>
+                <Transition
+                  enter="transition duration-100 ease-out"
+                  enterFrom="transform scale-95 opacity-0"
+                  enterTo="transform scale-100 opacity-100"
+                  leave="transition duration-75 ease-out"
+                  leaveFrom="transform scale-100 opacity-100"
+                  leaveTo="transform scale-95 opacity-0"
+                  className="w-full"
+                >
+                  <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
+                    <>
+                      {navigation.map((item, index) => (
+                        <Link
+                          key={index}
+                          href={`#${item.toLowerCase()}`}
+                          className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-trueGray-700"
+                        >
+                          {item}
+                        </Link>
+                      ))}
 
-                <Disclosure.Panel className="flex flex-wrap w-full my-5 lg:hidden">
-                  <>
-                    {navigation.map((item, index) => (
                       <Link
-                        key={index}
                         href="/"
-                        className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none dark:focus:bg-trueGray-700"
+                        className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5"
                       >
-                        {item}
+                        Get Started
                       </Link>
-                    ))}
-
-                    <Link
-                      href="/"
-                      className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5"
-                    >
-                      Get Started
-                    </Link>
-                  </>
-                </Disclosure.Panel>
+                    </>
+                  </Disclosure.Panel>
+                </Transition>
               </div>
             </>
           )}
@@ -75,7 +87,7 @@ export default function Navbar() {
             {navigation.map((menu, index) => (
               <li className="mr-3 nav__item" key={index}>
                 <Link
-                  href="/"
+                  href={`#${menu.toLowerCase()}`}
                   className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800"
                 >
                   {menu}
@@ -92,5 +104,5 @@ export default function Navbar() {
         </div>
       </nav>
     </div>
-  )
+  );
 }
---
+++ src/pages/index.tsx
@@ -0,0 +1,89 @@
+import Head from 'next/head';
+import { NextPage } from 'next';
+import { Fragment } from 'react';
+
+import Navbar from '@app/components/Navbar';
+import Hero from '@app/components/Hero';
+import SectionTitle from '@app/components/SectionTitle';
+import Benefits from '@app/components/Benefits';
+import { benefitOne, benefitTwo } from '@app/components/data';
+import OurTeam from '@app/components/OurTeam';
+import Footer from '@app/components/Footer';
+import Cta from '@app/components/Cta';
+
+type Props = {
+  animData: unknown;
+  animDataMobile: unknown;
+  animDataWeb: unknown;
+};
+
+const Home: NextPage<Props> = ({ animData, animDataMobile, animDataWeb }) => {
+  return (
+    <Fragment>
+      <Head>
+        <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
+        <meta
+          name="description"
+          content="Nextly is a free landing page template built with next.js & Tailwind CSS"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <Hero animData={animData} />
+      <SectionTitle pretitle="Tentang Kami" title="Modernize Digital Revolution">
+        Dengan tim berpengalaman, kami bergerak dibidang Teknologi Informasi dengan konsentrasi
+        pengembangan Aplikasi dan Sistem Informasi berbasis Web maupun Mobile yang memanfaatkan
+        teknologi modern dengan keterampilan digital untuk membantu dan mempercepat pertumbuhan
+        bisnis / organisasi Anda.
+      </SectionTitle>
+
+      <SectionTitle title="Layanan Kami" pretitle="Services" />
+      <Benefits animData={animDataMobile} data={benefitOne} />
+      <Benefits animData={animDataWeb} imgPos="right" data={benefitTwo} />
+      <SectionTitle pretitle="Tim" title="Tim pengembang kami" />
+      <OurTeam />
+      {/* 
+      
+     
+      <Benefits imgPos="right" data={benefitTwo} />
+   
+     
+      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
+        Answer your customers possible questions here, it will increase the conversion rate as well
+        as support or chat requests.
+      </SectionTitle>
+      <Faq />
+  
+     
+      <PopupWidget /> */}
+      <Cta />
+      <Footer />
+    </Fragment>
+  );
+};
+
+export async function getStaticProps() {
+  const [hero, web, mobile] = await Promise.all([
+    fetch('https://assets4.lottiefiles.com/packages/lf20_ypej3gd9.json'),
+    fetch('https://assets7.lottiefiles.com/packages/lf20_ksrcyxgn.json'),
+    fetch('https://assets5.lottiefiles.com/packages/lf20_wloxwco0.json'),
+  ]);
+
+  return {
+    props: {
+      animData: await hero.json(),
+      animDataMobile: await mobile.json(),
+      animDataWeb: await web.json(),
+    },
+  };
+}
+
+export default Home;
+
+// export default function Home({ animData }): NextPage<Props> {
+//   return (
+//     <>
+
+//     </>
+//   );
+// }
