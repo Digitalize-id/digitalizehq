bf122055458872eb0e4c8e641aaeeae49f1406b6
IyanSR <ikhyan7@gmail.com>
2023-03-29 11:43:27 +0800

    fix

--- src/components/CFImage.tsx
+++ src/components/CFImage.tsx
@@ -1,23 +1,23 @@
 import Image, { type ImageProps } from 'next/image';
 
-type Args = {
-  src: string;
-  width?: number | string;
-  quality?: number | string;
-};
+// type Args = {
+//   src: string;
+//   width?: number | string;
+//   quality?: number | string;
+// };
 
-const normalizeSrc = (src: string) => {
-  return src.startsWith('/') ? src.slice(1) : src;
-};
+// const normalizeSrc = (src: string) => {
+//   return src.startsWith('/') ? src.slice(1) : src;
+// };
 
-const cloudflareLoader = ({ src, width, quality }: Args) => {
-  const params = [`width=${width}`];
-  if (quality) {
-    params.push(`quality=${quality}`);
-  }
-  const paramsString = params.join(',');
-  return `/cdn-cgi/image/${paramsString}/${normalizeSrc(src)}`;
-};
+// const cloudflareLoader = ({ src, width, quality }: Args) => {
+//   const params = [`width=${width}`];
+//   if (quality) {
+//     params.push(`quality=${quality}`);
+//   }
+//   const paramsString = params.join(',');
+//   return `/cdn-cgi/image/${paramsString}/${normalizeSrc(src)}`;
+// };
 
 const CFImage = (props: ImageProps) => {
   return <Image {...props} />;
